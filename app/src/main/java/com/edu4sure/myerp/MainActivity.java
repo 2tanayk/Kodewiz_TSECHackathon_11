2cb6df6d416266ea08da984ad3307e83f15bd8fd
Tanay Kamath <2tanaykamath@gmail.com>
2020-07-02 19:12:14 +0530

    bug commit

--- app/src/main/java/com/edu4sure/myerp/MainActivity.java
+++ app/src/main/java/com/edu4sure/myerp/MainActivity.java
@@ -2,14 +2,37 @@ package com.edu4sure.myerp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn=(Button)findViewById(R.id.button);
+
+     /*   loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+              //  Intent i=new Intent(MainActivity.this,RetailerActivity.class);
+              //  MainActivity.this.startActivity(i);
+                 open();
+            }
+        });*/
+
+    }
+
+    public void open(View view)
+    {
+        Intent i=new Intent(this,RetailerActivity.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/edu4sure/myerp/RetailerActivity.java
@@ -0,0 +1,14 @@
+package com.edu4sure.myerp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RetailerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retailer);
+    }
+}
